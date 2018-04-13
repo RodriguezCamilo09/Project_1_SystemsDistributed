f882e5d16ded5565fd2d73ef979dc8864c850146
Camilo <cristian.rodriguez03@uptc.edu.co>
2018-04-13 15:39:17 -0500

    recevClient

--- Server.cpp
+++ Server.cpp
@@ -36,3 +36,42 @@ void Server::initializeServer(){
 	if(descriptorServer==-1 && idBind==-1)
 		cout<<"Error iniciando el servidor: "<<endl;
 }
+
+/**
+ * [Server::closeServer description]
+ */
+void Server::closeServer(){
+	exit(EXIT_SUCCESS);
+}
+
+void * recevClient(void * ser){
+	Server * server = (Server *) ser;
+
+	vector<ClientInfo *> listClients = server->getClientesTemporal();
+	cout<<"ListClients"<<endl;
+	CllientInfo * client = (ClientInfo * listClients[server->getCountClients()-1]);
+	char menssageOfClient[128];
+	char key[]= "1";
+
+	while(client->getState()){
+		cout<<"Listen" <<endl;
+		int i=recv(client->getDescriptorClient(), (void *)&menssageOfClient,128,0);
+		cout<<"Listen" <<endl;
+		sleep(1);
+		if(i!=0){
+			if(strcmp (key,menssageOfClient) != 0){
+				recv(client->getDescriptorClient(), (void *)&menssageOfClient,128,0);
+				cout<<menssageOfClient<<endl;
+				vector <ClientInfo *> seleccion = server->seleccionarStorage();
+				server->recevibirArchivo((void *)cliente,(void *) seleccion[0],(void *)seleccion[1], (void *) mensajeDeCliente);
+			}
+		}else{
+			cout<<"Se desconecto el cliente con IP: "<<inet_ntoa(cliente->getClienteInfor().sin_addr)<<" con error "<<endl;
+			cliente->setEstado(false);
+			close(cliente->getDescriptorCliente());
+		}
+	}
+
+}
+lunes 8:30am
+casa de mujer
